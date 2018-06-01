4638aabc19d7fe2a4d71bb670ac5ed6b7f3dc438
Krishna <krishnamurugan589@gmail.com>
2018-06-01 11:32:27 +0530

    Aligned buttons for small screens

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     let colors = ['#E27D60','#85DCB','#E8A87C','#C38D9E','#41B3A3','F64C72','#99738E','#659DBD','##DAAD86'];
     let rand = Math.floor(Math.random() * colors.length);
     $.ajax({
-        url:'http://api.forismatic.com/api/1.0/',
+        url:'https://api.forismatic.com/api/1.0/',
         jsonp:'jsonp',
         dataType:'jsonp',
         data:{
@@ -30,7 +30,7 @@ $(document).ready(function(){
         event.preventDefault();
         let rand = Math.floor(Math.random() * colors.length);
         $.ajax({
-            url:'http://api.forismatic.com/api/1.0/',
+            url:'https://api.forismatic.com/api/1.0/',
             jsonp:'jsonp',
             dataType:'jsonp',
             data:{
